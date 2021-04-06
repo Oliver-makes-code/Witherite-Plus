c83d16829f26b3556d4de6d25fe77cb71244ae62
FlamingCherry <81635332+FlamingCherry@users.noreply.github.com>
2021-04-06 12:14:10 +0300

    Update Register.java
    Made witherite items fireproof

--- src/main/java/flamingcherry/witherite/Register.java
+++ src/main/java/flamingcherry/witherite/Register.java
@@ -11,15 +11,15 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Register {
-    public static final Item WITHERITE = new flamingcherry.witherite.items.Witherite(new FabricItemSettings().group(Witherite.WITHERITE_GROUP));
-    public static final Item WITHERITE_INGOT = new WitheriteIngot(new Item.Settings().group(Witherite.WITHERITE_GROUP));
-    public static ToolItem WITHERITE_SHOVEL = new ShovelItem(WitheriteToolMaterial.INSTANCE, 2, -3.0F, new Item.Settings().group(Witherite.WITHERITE_GROUP));
-    public static ToolItem WITHERITE_SWORD = new SwordItem(WitheriteToolMaterial.INSTANCE, 5, -1.0F, new Item.Settings().group(Witherite.WITHERITE_GROUP));
-    public static ToolItem WITHERITE_PICKAXE = new WitheritePickaxe(WitheriteToolMaterial.INSTANCE, 3, -2.0F, new Item.Settings().group(Witherite.WITHERITE_GROUP));
-    public static ToolItem WITHERITE_AXE = new WitheriteAxe(WitheriteToolMaterial.INSTANCE, 6, -2.5F, new Item.Settings().group(Witherite.WITHERITE_GROUP));
-    public static ToolItem WITHERITE_HOE = new WitheriteHoe(WitheriteToolMaterial.INSTANCE, 2, -0.50F, new Item.Settings().group(Witherite.WITHERITE_GROUP));
-    public static ShearsItem WITHERITE_SHEARS = new WitheriteShears(new FabricItemSettings().maxDamage(1000).group(Witherite.WITHERITE_GROUP));
-    public static FlintAndSteelItem WITHERITE_FLINT_AND_STEEL = new WitheriteFlintAndSteel(new FabricItemSettings().maxDamage(856).group(Witherite.WITHERITE_GROUP));
+    public static final Item WITHERITE = new flamingcherry.witherite.items.Witherite(new FabricItemSettings().group(Witherite.WITHERITE_GROUP).fireproof());
+    public static final Item WITHERITE_INGOT = new WitheriteIngot(new Item.Settings().group(Witherite.WITHERITE_GROUP).fireproof());
+    public static ToolItem WITHERITE_SHOVEL = new ShovelItem(WitheriteToolMaterial.INSTANCE, 2, -3.0F, new Item.Settings().group(Witherite.WITHERITE_GROUP).fireproof());
+    public static ToolItem WITHERITE_SWORD = new SwordItem(WitheriteToolMaterial.INSTANCE, 5, -1.0F, new Item.Settings().group(Witherite.WITHERITE_GROUP).fireproof());
+    public static ToolItem WITHERITE_PICKAXE = new WitheritePickaxe(WitheriteToolMaterial.INSTANCE, 3, -2.0F, new Item.Settings().group(Witherite.WITHERITE_GROUP).fireproof());
+    public static ToolItem WITHERITE_AXE = new WitheriteAxe(WitheriteToolMaterial.INSTANCE, 6, -2.5F, new Item.Settings().group(Witherite.WITHERITE_GROUP).fireproof());
+    public static ToolItem WITHERITE_HOE = new WitheriteHoe(WitheriteToolMaterial.INSTANCE, 2, -0.50F, new Item.Settings().group(Witherite.WITHERITE_GROUP).fireproof());
+    public static ShearsItem WITHERITE_SHEARS = new WitheriteShears(new FabricItemSettings().maxDamage(1000).group(Witherite.WITHERITE_GROUP).fireproof());
+    public static FlintAndSteelItem WITHERITE_FLINT_AND_STEEL = new WitheriteFlintAndSteel(new FabricItemSettings().maxDamage(856).group(Witherite.WITHERITE_GROUP).fireproof());
     public static final Tag<Item> SHEARS_ITEM = FabricToolTags.SHEARS;
 
     public static void register() {
